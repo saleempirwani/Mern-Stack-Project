6526fe419be08a57bd3de0e10e13f8903396a877
saleempirwani <salimraza3465@gmail.com>
2022-04-24 00:10:46 +0500

    update

--- BlogApp/server/db/index.js
+++ BlogApp/server/db/index.js
@@ -4,7 +4,6 @@ mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,    
   })
   .then(() => {
     console.log("MongoDB connected");
